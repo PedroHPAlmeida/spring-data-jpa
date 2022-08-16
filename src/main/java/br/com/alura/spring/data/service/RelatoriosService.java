ce43147c6d7e7f8bf450536bcfa1f38479d723bd
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-15 21:43:00 -0300

    Aula 03 - JPQL

--- src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
+++ src/main/java/br/com/alura/spring/data/service/RelatoriosService.java
@@ -4,6 +4,10 @@ import br.com.alura.spring.data.orm.Funcionario;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,6 +15,7 @@ import java.util.Scanner;
 public class RelatoriosService {
 
     private boolean system = true;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private final FuncionarioRepository repository;
 
     public RelatoriosService(FuncionarioRepository repository) {
@@ -22,12 +27,16 @@ public class RelatoriosService {
             System.out.println("Qual acao de cargo deseja executar");
             System.out.println("0 - Sair");
             System.out.println("1 - Busca funcionário nome");
+            System.out.println("2 - Busca funcionário nome, data contratacao e salario maior que");
 
             int action = scanner.nextInt();
 
             switch (action) {
                 case 1:
                     buscaFuncionarioNome(scanner);
+                    break;
+                case 2:
+                    buscaFuncionarioNomeSalarioMaiorData(scanner);
                     break;
                 default:
                     system = false;
@@ -40,6 +49,20 @@ public class RelatoriosService {
         System.out.println("Qual nome deseja pesquisar?");
         String nome = scanner.next();
         List<Funcionario> list = repository.findByNome(nome);
+        list.forEach(System.out::println);
+    }
+
+    private void buscaFuncionarioNomeSalarioMaiorData(Scanner scanner){
+        System.out.println("Qual nome deseja pesquisar?");
+        String nome = scanner.next();
+
+        System.out.println("Qual data contratacao deseja pesquisar?");
+        LocalDate data = LocalDate.parse(scanner.next(), formatter);
+
+        System.out.println("Qual salario deseja pesquisar?");
+        BigDecimal salario = new BigDecimal(scanner.next());
+
+        List<Funcionario> list = repository.findNomeSalarioMaiorDataContratacao(nome, salario, data);
         list.forEach(System.out::println);
     }
 }
