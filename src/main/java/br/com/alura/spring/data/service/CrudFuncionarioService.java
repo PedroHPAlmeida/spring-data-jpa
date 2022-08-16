21d65f4bd0044873b2475b9f9bb8a3f3dcaee79e
PedroHPAlmeida <pedro.6571almeida@gmail.com>
2022-08-16 18:31:38 -0300

    Aula 04 - Paginacao

--- src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
+++ src/main/java/br/com/alura/spring/data/service/CrudFuncionarioService.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Cargo;
@@ -53,7 +57,7 @@ public class CrudFuncionarioService {
 				atualizar(scanner);
 				break;
 			case 3:
-				visualizar();
+				visualizar(scanner);
 				break;
 			case 4:
 				deletar(scanner);
@@ -149,9 +153,17 @@ public class CrudFuncionarioService {
         System.out.println("Alterado");
 	}
 	
-	private void visualizar() {
-		Iterable<Funcionario> funcionarios = funcionarioRepository.findAll();
-		funcionarios.forEach(funcionario -> System.out.println(funcionario));
+	private void visualizar(Scanner scanner) {
+		System.out.println("Qual página você deseja visualizar?");
+		Integer page = scanner.nextInt();
+
+		Pageable pageable = PageRequest.of(page,5, Sort.unsorted());
+
+		Page<Funcionario> funcionarios = funcionarioRepository.findAll(pageable);
+		System.out.println(funcionarios);
+		System.out.println("Página atual " + funcionarios.getNumber());
+		System.out.println("Total de elementos " + funcionarios.getTotalElements());
+		funcionarios.forEach(System.out::println);
 	}
 	
 	private void deletar(Scanner scanner) {
